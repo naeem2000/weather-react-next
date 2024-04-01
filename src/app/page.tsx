2a7c46fbd6f86cf3e42a318749f923d6c8e512f9
Naeem Carr <mogamatnaeemcarr@gmail.com>
2024-04-01 15:15:46 +0200

    added better styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,59 +16,82 @@ export default function Home() {
 	const [currentWeather, setCurrentWeather] = useState<any>();
 	//state for query from input
 	const [query, setQuery] = useState<string>('');
+	//state for date and time
+	const [time, setTime] = useState('');
+	const [date, setDate] = useState('');
 
 	const route = useRouter();
 
 	useEffect(() => {
 		//store what is currently in local storage
 		const currentDay = localStorage.getItem('days');
-		const currentWeather = localStorage.getItem('weather');
+		const wholeWeather = localStorage.getItem('weather');
+		const currentWeather = localStorage.getItem('current');
 
 		//fetching whats in local storage and updating states accordingly
-		if (currentDay && currentWeather) {
+		if (currentDay && wholeWeather && currentWeather) {
+			setCurrentWeather(JSON.parse(currentWeather));
 			setDays(JSON.parse(currentDay));
-			setWeather(JSON.parse(currentWeather));
+			setWeather(JSON.parse(wholeWeather));
 		}
+
+		//date and time for header
+		const dateAndTime = () => {
+			const now = new Date();
+			const formatDate = new Intl.DateTimeFormat('en-US', {
+				weekday: 'long',
+				year: 'numeric',
+				month: 'long',
+				day: '2-digit',
+			}).format(now);
+			const formatTime = new Intl.DateTimeFormat('en-US', {
+				hour: '2-digit',
+				minute: '2-digit',
+				hour12: false,
+			}).format(now);
+
+			setTime(formatTime);
+			setDate(formatDate);
+		};
+		dateAndTime();
 	}, []);
 
 	const search = async (e: any) => {
 		//if a query has been submitted within input field
 		if (query) {
 			//if enter key has been pressed
-			if (e.key === 'Enter') {
-				//awaiting response from API
-				const response = await fetch(
-					`${api.base}forecast?q=${query}&units=metric&APPID=${api.key}`
-				);
+			//awaiting response from API
+			const response = await fetch(
+				`${api.base}forecast?q=${query}&units=metric&APPID=${api.key}`
+			);
 
-				//awaiting response from API to parse into JSON
-				const result = await response.json();
+			//awaiting response from API to parse into JSON
+			const result = await response.json();
 
-				//awaiting response from current weather endpoint
-				const currentResponse = await fetch(
-					`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`
-				);
+			//awaiting response from current weather endpoint
+			const currentResponse = await fetch(
+				`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`
+			);
 
-				//awaiting response from current weather endpoint to parse into JSON
-				const currentResult = await currentResponse.json();
+			//awaiting response from current weather endpoint to parse into JSON
+			const currentResult = await currentResponse.json();
 
-				//updating states after asynchronous steps
-				setCurrentWeather(currentResult);
-				setWeather(result);
-				setQuery('');
+			//updating states after asynchronous steps
+			setCurrentWeather(currentResult);
+			setWeather(result);
+			setQuery('');
 
-				//filtering that only 1 array be taken for every 10 as the resonse returns 40 arrays containing the same day.
-				const tenDays = result.list?.filter(
-					(_: any, index: number) => (index + 1) % 8 === 0
-				);
+			//filtering that only 1 array be taken for every 10 as the resonse returns 40 arrays containing the same day.
+			const tenDays = result.list?.filter(
+				(_: any, index: number) => (index + 1) % 8 === 0
+			);
 
-				//updating state after filtering for days state and updating local storage
-				setDays(tenDays);
-				localStorage.setItem('weather', JSON.stringify(result));
-				localStorage.setItem('days', JSON.stringify(tenDays));
-				localStorage.setItem('query', query);
-				localStorage.setItem('current', JSON.stringify(currentWeather));
-			}
+			//updating state after filtering for days state and updating local storage
+			setDays(tenDays);
+			localStorage.setItem('weather', JSON.stringify(result));
+			localStorage.setItem('days', JSON.stringify(tenDays));
+			localStorage.setItem('query', query);
+			localStorage.setItem('current', JSON.stringify(currentWeather));
 		} else {
 			//error if input is empty
 			console.log('please enter city');
@@ -79,45 +102,51 @@ export default function Home() {
 	const goDay = (date: string) => {
 		route.push(`/hourly?date=${date}`);
 	};
-
+	console.log(weather);
 	return (
 		<>
 			<main>
-				<div className='search-box'>
-					<input
-						type='text'
-						placeholder='Search...'
-						onChange={(e) => setQuery(e.target.value)}
-						value={query}
-						onKeyDown={search}
-					/>
+				<div className='weather-header'>
+					<div className='country'>
+						{weather && <h1>{weather.city.name}</h1>}
+						<p>
+							{date} &nbsp; | &nbsp; {time}
+						</p>
+					</div>
+					<div className='input-side'>
+						<Image src={'/search.png'} width={30} height={30} alt='search' />
+						<input
+							type='text'
+							placeholder='Search...'
+							onChange={(e) => setQuery(e.target.value)}
+							value={query}
+						/>
+						<button onClick={search}>Enter</button>
+					</div>
+				</div>
+				<div className='current-weather'>
+					{currentWeather && (
+						<div className='current-weather'>
+							<h2>Today</h2>
+							<div className='temps'>
+								<p>Current: {Math.round(currentWeather.main.temp)}</p>
+								<p>Min: {Math.round(currentWeather.main.temp_min)}</p>
+								<p>Max: {Math.round(currentWeather.main.temp_max)}</p>
+							</div>
+							<div className='description'>
+								<p>{currentWeather.weather[0].description}</p>
+							</div>
+							<Image
+								src={`https://openweathermap.org/img/w/${currentWeather.weather[0].icon}.png`}
+								height={50}
+								width={50}
+								alt={currentWeather.weather[0].description}
+							/>
+						</div>
+					)}
 				</div>
 				{weather && weather.city ? (
 					<div className='weather'>
-						<div className='location-box'>
-							<h1>
-								{weather.city?.name}, {weather?.city.country}
-							</h1>
-						</div>
-						{currentWeather && (
-							<div className='current-weather'>
-								<h2>Today</h2>
-								<div className='temps'>
-									<p>Current: {Math.round(currentWeather.main.temp)}</p>
-									<p>Min: {Math.round(currentWeather.main.temp_min)}</p>
-									<p>Max: {Math.round(currentWeather.main.temp_max)}</p>
-								</div>
-								<div className='description'>
-									<p>{currentWeather.weather[0].description}</p>
-								</div>
-								<Image
-									src={`https://openweathermap.org/img/w/${currentWeather.weather[0].icon}.png`}
-									height={50}
-									width={50}
-									alt={currentWeather.weather[0].description}
-								/>
-							</div>
-						)}
 						<div className='weather-box'>
 							{days?.map((day: any, index: any) => (
 								<div className='box' onClick={() => goDay(day.dt)} key={index}>
@@ -131,7 +160,6 @@ export default function Home() {
 												Max: {Math.round(day.main.temp_max)}&deg;C
 											</p>
 										</div>
-										<p>Feels like: {Math.round(day.main.feels_like)}&deg;C</p>
 										<div className='description'>
 											<p>{day.weather[0].description}</p>
 										</div>

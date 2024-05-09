f795b9398065117fc3e548f017828f5fc66e428a
Naeem Carr <mogamatnaeemcarr@gmail.com>
2024-05-09 15:40:58 +0200

    Added error handing of no location found

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -142,10 +142,11 @@ export default function Home() {
 					<Loader />
 				) : (
 					<>
-						{' '}
 						<div className='weather-header'>
 							<div className='country'>
-								{weather && <h1>{weather.city.name}</h1>}
+								{weather?.message !== 'city not found' && (
+									<>{weather && <h1>{weather.city.name}</h1>}</>
+								)}
 								<p>
 									{date} &nbsp; | &nbsp; {time}
 								</p>
@@ -171,88 +172,97 @@ export default function Home() {
 								<button onClick={search}>Enter</button>
 							</div>
 						</div>
-						{currentWeather && (
-							<>
-								<div className='temps'>
-									<div className='temp-left'>
-										<p>{Math.round(currentWeather.main.temp)}</p>
-										<div>
-											<span>&deg;C</span>
-											<br />
-											<span>{currentWeather.weather[0].description}</span>
-										</div>
-									</div>
-									<div className='temp-right'>
-										<Image
-											src={`https://openweathermap.org/img/wn/${currentWeather.weather[0].icon}@2x.png`}
-											height={50}
-											width={50}
-											alt={currentWeather.weather[0].description}
-										/>
-										<div className='condition-rows'>
-											<div>
-												<Image
-													src={'/feels.png'}
-													width={20}
-													height={20}
-													alt='feels like'
-												/>
-												<Image
-													src={'/humid.png'}
-													width={20}
-													height={20}
-													alt='humidity'
-												/>
-												<Image
-													src={'/wind.png'}
-													width={20}
-													height={20}
-													alt='wind'
-												/>
-											</div>
-											<div>
-												<p>
-													Feels like:{' '}
-													{Math.round(currentWeather.main.feels_like)}
-													&deg;C
-												</p>
-												<p>
-													Humidity: {Math.round(currentWeather.main.humidity)}%
-												</p>
-												<p>Wind: {Math.round(currentWeather.wind.speed)}km/h</p>
-											</div>
-										</div>
-									</div>
-								</div>
-							</>
-						)}
-						{weather && weather.city ? (
-							<div className='weather'>
-								<div className='weather-box'>
-									{days?.map((day: any, index: any) => (
-										<div
-											className='box'
-											onClick={() => goDay(day.dt)}
-											key={index}
-										>
-											<h2>{makeDay(day.dt_txt)}</h2>
-											<Image
-												src={`https://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png`}
-												height={90}
-												width={90}
-												alt={day.weather[0].description}
-											/>
-											<p>
-												{Math.round(day.main.temp_min)}&deg; -{' '}
-												{Math.round(day.main.temp_max)}&deg;
-											</p>
-											<p>{day.weather[0].description}</p>
-										</div>
-									))}
-								</div>
-							</div>
+						{weather?.message === 'city not found' ? (
+							<h1 className='no-city'>Location not found</h1>
 						) : (
-							<h1 className='no-city'>No location selected</h1>
+							<>
+								{currentWeather && (
+									<>
+										<div className='temps'>
+											<div className='temp-left'>
+												<p>{Math.round(currentWeather.main.temp)}</p>
+												<div>
+													<span>&deg;C</span>
+													<br />
+													<span>{currentWeather.weather[0].description}</span>
+												</div>
+											</div>
+											<div className='temp-right'>
+												<Image
+													src={`https://openweathermap.org/img/wn/${currentWeather.weather[0].icon}@2x.png`}
+													height={50}
+													width={50}
+													alt={currentWeather.weather[0].description}
+												/>
+												<div className='condition-rows'>
+													<div>
+														<Image
+															src={'/feels.png'}
+															width={20}
+															height={20}
+															alt='feels like'
+														/>
+														<Image
+															src={'/humid.png'}
+															width={20}
+															height={20}
+															alt='humidity'
+														/>
+														<Image
+															src={'/wind.png'}
+															width={20}
+															height={20}
+															alt='wind'
+														/>
+													</div>
+													<div>
+														<p>
+															Feels like:{' '}
+															{Math.round(currentWeather.main.feels_like)}
+															&deg;C
+														</p>
+														<p>
+															Humidity:{' '}
+															{Math.round(currentWeather.main.humidity)}%
+														</p>
+														<p>
+															Wind: {Math.round(currentWeather.wind.speed)}km/h
+														</p>
+													</div>
+												</div>
+											</div>
+										</div>
+									</>
+								)}
+								{weather && weather.city ? (
+									<div className='weather'>
+										<div className='weather-box'>
+											{days?.map((day: any, index: any) => (
+												<div
+													className='box'
+													onClick={() => goDay(day.dt)}
+													key={index}
+												>
+													<h2>{makeDay(day.dt_txt)}</h2>
+													<Image
+														src={`https://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png`}
+														height={90}
+														width={90}
+														alt={day.weather[0].description}
+													/>
+													<p>
+														{Math.round(day.main.temp_min)}&deg; -{' '}
+														{Math.round(day.main.temp_max)}&deg;
+													</p>
+													<p>{day.weather[0].description}</p>
+												</div>
+											))}
+										</div>
+									</div>
+								) : (
+									<h1 className='no-city'>No location selected</h1>
+								)}
+							</>
 						)}
 					</>
 				)}
